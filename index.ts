3f370dfb33fd340a1150dd2fa948c29cf8c38508
Masaya Nakamura <mashabow@shiromoji.net>
2022-11-12 21:59:27 +0900

    feat: open a web page on Chromium with Playwright Library
    https://playwright.dev/docs/library#first-script

---
+++ index.ts
@@ -0,0 +1,9 @@
+import { chromium } from "playwright";
+
+(async () => {
+  const browser = await chromium.launch({ headless: false, slowMo: 50 });
+  const page = await browser.newPage();
+  await page.goto("http://whatsmyuseragent.org/");
+  await page.screenshot({ path: "example.png" });
+  await browser.close();
+})();
