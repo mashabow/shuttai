0fe6b57f062299ab2f7444556dcba97c64a92bab
Masaya Nakamura <mashabow@shiromoji.net>
2022-11-14 21:21:24 +0900

    feat: add a development flag

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { chromium } from "playwright";
 
 const STORAGE_STATE_PATH = "auth.json";
 
+const isDev = Boolean(process.env.DEV);
+
 (async () => {
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext({
@@ -14,11 +16,15 @@ const STORAGE_STATE_PATH = "auth.json";
   const sendButton = page.getByRole("button", { name: "Send now" });
 
   // チャネルを開いて「おはよう」を入力
-  await page.goto("https://app.slack.com/client/T02CW9KLLBD/D02CWBKTH8S");
+  await page.goto(
+    isDev
+      ? "https://app.slack.com/client/T02CW9KLLBD/D02CWBKTH8S"
+      : "https://app.slack.com/client/T02CW9KLLBD/C047WUSRDSM"
+  );
   await textbox.fill(":ohayo:");
   await textbox.focus();
 
-  // クリックを待つ
+  // 手動でのクリックを待つ
   await sendButton.evaluate(async (el) => {
     el.insertAdjacentHTML("afterbegin", "出勤");
     return new Promise((resolve) => el.addEventListener("click", resolve));
@@ -28,11 +34,15 @@ const STORAGE_STATE_PATH = "auth.json";
   await page.goto("https://app.slack.com/client/T02CW9KLLBD/D02D05JQ4V9");
   await page.getByRole("textbox").fill("/freee_dakoku");
   await sendButton.click();
-  // // 出勤ボタンをクリック
-  // await page.getByRole("button", { name: "出勤" }).click();
+  // 出勤ボタンをクリック
+  if (!isDev) {
+    // TODO: 複数あった場合のハンドリング
+    await page.getByRole("button", { name: "出勤" }).click();
+  }
 
-  // Pause the page, and start recording manually.
-  await page.pause();
+  if (isDev) {
+    await page.pause();
+  }
 
   // ---------------------
   await context.storageState({ path: STORAGE_STATE_PATH });
