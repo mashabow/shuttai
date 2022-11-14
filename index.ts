def71fdea373062575330cf50f61a4bf2cbf2e03
Masaya Nakamura <mashabow@shiromoji.net>
2022-11-15 08:53:29 +0900

    feat: do dakoku first and simplify

--- index.ts
+++ index.ts
@@ -15,40 +15,27 @@ const isDev = Boolean(process.env.DEV);
   const textbox = page.getByRole("textbox");
   const sendButton = page.getByRole("button", { name: "Send now" });
 
-  // チャネルを開いて「おはよう」を入力
+  // 「人事労務freee」アプリで出勤の打刻をする
+  await page.goto("https://app.slack.com/client/T02CW9KLLBD/D02D05JQ4V9");
+  await textbox.fill("/freee_dakoku");
+  await sendButton.click();
+  if (!isDev) {
+    const shukkinButtonHandle = await page
+      .getByRole("button", { name: "出勤" })
+      .elementHandle();
+    await shukkinButtonHandle?.click();
+    await shukkinButtonHandle?.waitForElementState("hidden");
+  }
+
+  // daily チャンネルを開いて「おはよう」を投稿
   await page.goto(
     isDev
       ? "https://app.slack.com/client/T02CW9KLLBD/D02CWBKTH8S"
       : "https://app.slack.com/client/T02CW9KLLBD/C047WUSRDSM"
   );
   await textbox.fill(":ohayo:");
-  await textbox.focus();
-
-  // 手動でのクリックを待つ
-  await sendButton.evaluate(async (el) => {
-    el.insertAdjacentHTML("afterbegin", "出勤");
-    return new Promise((resolve) => el.addEventListener("click", resolve));
-  });
-
-  // 「人事労務freee」アプリで出勤の打刻をする
-
-  await page.goto("https://app.slack.com/client/T02CW9KLLBD/D02D05JQ4V9");
-  // 最後のメッセージの ID を取得
-  const lastMessageId = await page
-    .getByRole("listitem")
-    .last()
-    .getAttribute("id");
-  // スラッシュコマンド `/freee_dakoku` を実行
-  await textbox.fill("/freee_dakoku");
   await sendButton.click();
-
-  // 新たな [出勤] ボタンの出現を待ってクリック
-  const shukkinButton = page
-    .locator(`${lastMessageId} + *`)
-    .getByRole("button", { name: "出勤" });
-  if (!isDev) {
-    await shukkinButton.click();
-  }
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   if (isDev) {
     await page.pause();
