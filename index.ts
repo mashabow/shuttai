ab64f5b845cf9b9277f9cd1506fb046cce0e02bf
Masaya Nakamura <mashabow@shiromoji.net>
2023-02-27 21:41:40 +0900

    feat: add taikin command

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import { chromium } from "playwright";
 const STORAGE_STATE_PATH = "auth.json";
 
 const isDev = Boolean(process.env.DEV);
+const isShukkin = process.argv[2] === "shukkin";
+
+console.log(process.argv);
 
 (async () => {
   const browser = await chromium.launch({ headless: false });
@@ -20,20 +23,20 @@ const isDev = Boolean(process.env.DEV);
   await textbox.fill("/freee_dakoku");
   await sendButton.click();
   if (!isDev) {
-    const shukkinButtonHandle = await page
-      .getByRole("button", { name: "出勤" })
+    const buttonHandle = await page
+      .getByRole("button", { name: isShukkin ? "出勤" : "退勤" })
       .elementHandle();
-    await shukkinButtonHandle?.click();
-    await shukkinButtonHandle?.waitForElementState("hidden");
+    await buttonHandle?.click();
+    await buttonHandle?.waitForElementState("hidden");
   }
 
-  // daily チャンネルを開いて「おはよう」を投稿
+  // daily チャンネルを開いて挨拶を投稿
   await page.goto(
     isDev
       ? "https://app.slack.com/client/T02CW9KLLBD/D02CWBKTH8S"
       : "https://app.slack.com/client/T02CW9KLLBD/C047WUSRDSM"
   );
-  await textbox.fill(":ohayo:");
+  await textbox.fill(isShukkin ? ":ohayo:" : ":otukaresamadesu:");
   await sendButton.click();
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
